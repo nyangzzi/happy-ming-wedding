b5e60787d455fcced6c6cbbfb89b217e33106f68
l2k <ekyi0421@gmail.com>
2025-11-05 00:48:11 +0900

    build: git url 배포 환경 세팅

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createHashRouter } from 'react-router-dom'
 
 import GamePage from '../pages/GamePage'
 import HomePage from '../pages/HomePage'
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   { path: '/', element: <HomePage /> },
   { path: '/game', element: <GamePage /> },
 ])
--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './game.css'
 import groomSu from '../assets/groom-su.png'
