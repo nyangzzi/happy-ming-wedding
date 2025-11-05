4fdad8164656887fe8e711f2e16d1677fcceed93
eunkyung.lee <eklee98@lotte.net>
2025-11-05 17:51:23 +0900

    refactor: 홈 이미지 공통화

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
       <div style={{height: 100}}/>
       <p>어쩌다 이렇게 된 걸까요?</p>
       <p>그건 아마도..</p>
-      <img src={kakaotalkImg} alt="카카오톡" style={{ width: '100%', height: 'auto' }} loading="lazy" />
+      <img src={kakaotalkImg} alt="카카오톡" loading="lazy" />
       <p>생각치도 못한 카톡 한 통에서 시작되었던 거에요</p>
       <div className="vertical-dots">
         <span>.</span>
@@ -26,22 +26,22 @@ export default function HomePage() {
    
       <div className="flex-col-center">
         {'<지금 시작합니다>'}
-        <img src={posterImg} alt="환연포스터" style={{ width: '100%', height: 'auto' }} loading="lazy" />
+        <img src={posterImg} alt="환연포스터" loading="lazy" />
       </div>
 
       <div style={{textAlign: 'center' ,}} className="flex-col-center">
         환승연애 시즌 ∞ !<br/> 새로운 하우스에 입주가 시작되었습니다
-        <img src={house} alt="환연하우스" style={{ width: '100%', height: 'auto' }} loading="lazy" />
+        <img src={house} alt="환연하우스" loading="lazy" />
       </div>
        
       <div className="flex-col-center">
         첫 번째 입주자, 박민경
-        <img src={girlMing} alt="박밍공" style={{ width: '100%', height: 'auto' }} loading="lazy" />
+        <img src={girlMing} alt="박밍공" loading="lazy" />
       </div>
 
       <div className="flex-col-center">
         두 번째 입주자, 임수철
-        <img src={boySu} alt="임수철" style={{ width: '100%', height: 'auto' }} loading="lazy" />
+        <img src={boySu} alt="임수철" loading="lazy" />
       </div>
       
       <p>~~어쩌구 저쩌구 게임도 있답니다~~</p>
